19e98139760999706483a84979c63bed12600c2e
Tommydemian <tomasgilamoedo@gmail.com>
2022-01-07 20:26:46 -0300

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import {initializeApp} from 'firebase'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMaezwLw1NrZs4JM7Oso4NhVaEqyegZYU",
+  authDomain: "fir-e-commerce-683f0.firebaseapp.com",
+  projectId: "fir-e-commerce-683f0",
+  storageBucket: "fir-e-commerce-683f0.appspot.com",
+  messagingSenderId: "534069272465",
+  appId: "1:534069272465:web:3481ad5a5beadbf1570430"
+};
+
+const app = initializeApp(firebaseConfig)
+
+
+
